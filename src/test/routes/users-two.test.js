3acc83995106b24d9b662325f6aff72b72709fc1
Valeriy Titov <iqronaft@gmail.com>
2024-03-02 14:21:22 +0200

    added method reset in repo

--- src/test/routes/users-two.test.js
+++ src/test/routes/users-two.test.js
@@ -1,24 +1,28 @@
-const request = require('supertest');
-const buildApp = require('../../app');
-const UserRepo = require('../../repos/user-repo');
-const pool = require('../../pool');
-const Context = require('../context');
+const request = require("supertest");
+const buildApp = require("../../app");
+const UserRepo = require("../../repos/user-repo");
+const pool = require("../../pool");
+const Context = require("../context");
 
 let context;
 beforeAll(async () => {
   context = await Context.build();
 });
 
+beforeEach(async () => {
+  await context.reset();
+});
+
 afterAll(() => {
   return context.close();
 });
 
-it('create a user', async () => {
+it("create a user", async () => {
   const startingCount = await UserRepo.count();
 
   await request(buildApp())
-    .post('/users')
-    .send({ username: 'testuser', bio: 'test bio' })
+    .post("/users")
+    .send({ username: "testuser", bio: "test bio" })
     .expect(200);
 
   const finishCount = await UserRepo.count();
